a043d2662a8830b9cf78612c9b5b0d9b114a3a0e
T.Hirai <flat3t@gmail.com>
2018-04-15 17:33:41 +0900

    Utitlityファイルを追加

---
+++ UnityProject/Assets/Scripts/TransUtility.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TransUtility
+{
+	public static Vector3 UpdateConcentricMove( Vector3 currentPosition, Vector3 centralPosition, Vector3 axis, float, radius, float degreePerSecond )
+	{
+		Vector3 moveVec = (currentPosition - centralPosition).Normalized() * radius;
+		moveVec = Quaternion.AngleAxis( degreePerSecond * Time.deltaTime, axis ) * moveVec;
+
+		Vector3 movedPos = (centralPosition + moveVec) - currentPosition;
+
+		return (movedPos - currentPosition);
+	}
+}
